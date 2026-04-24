b5bf09f65d4d7c3d155f2593a903bc3f74a5f2ad
akshad-exe <akshadjogi@gmail.com>
2026-04-24 07:05:58 +0530

    refactor: update import statements to use type annotations

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
-import clsx, { ClassValue } from "clsx";
+import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 function cn(...inputs: ClassValue[]) {
